5f7e1e95e89add502f5deb597271f5ba4d9f21a1
lliolla <lliolla@hotmail.com>
2023-05-27 14:26:38 +0200

    STYLE : style page memory

---
+++ src/data/memoryData.js
@@ -0,0 +1,51 @@
+export const cardArray =[
+    {
+        id:"1",
+        src :"./images/1.png"
+    },
+    {
+        id:"2",
+        src :"./images/2.png"
+    },
+    {
+        id:"3",
+        src :"./images/3.png"
+    },
+    {
+        id:"4",
+        src :"./images/4.png"
+    },
+    {
+        id:"5",
+        src :"./images/5.png"
+    },
+    {
+        id:"6",
+        src :"./images/6.png"
+    },
+    {
+        id:"1",
+        src :"./images/1.png"
+    },
+    {
+        id:"2",
+        src :"./images/2.png"
+    },
+    {
+        id:"3",
+        src :"./images/3.png"
+    },
+    {
+        id:"4",
+        src :"./images/4.png"
+    },
+    {
+        id:"5",
+        src :"./images/5.png"
+    },
+    {
+        id:"6",
+        src :"./images/6.png"
+    }
+    
+]
--- src/pages/Memory.jsx
+++ src/pages/Memory.jsx
@@ -1,9 +1,45 @@
-
+import { useState } from 'react';
+import {cardArray} from '../data/memoryData'
 
 const Memory = () => {
+const cardArrayLenght = cardArray.length
+const [cards, setcards] = useState([])
+const [turns, setturns] = useState(0)
+
+const shuffleCard = ()=>{
+    const shuffleArray =[...cardArray,...cardArray] //  create array of pairs of images
+    //sort image in the news array
+    .sort(()=>Math.random()-0.5 )
+    .map((card) =>({...card,id:Math.random()}))
+
+    setcards(shuffleArray)
+    setturns(0)
+    }   
+console.log("shuffleArray",cards,turns);
+    /* gird memomry */
+           /* card component */
+          /* match card fonction */
+           /* flip card with css */
     return (
-        <div>
-          Memory  
+        <div className="game-container">
+            
+            <header>
+            <h1> Jeu de memory</h1> 
+            </header>
+          <section className="score-board" >
+          <p>score :</p>
+          <p>nombre de coup </p>
+          <p>Memory lenght :  
+           {cardArrayLenght}</p>
+           <button className="button" onClick={shuffleCard}>nouveau jeux</button>
+
+          </section>
+        <section className="game-gird">
+        
+        </section>
+          
+
+          
         </div>
     );
 };
