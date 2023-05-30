0727e827c709ce91e7530a379f83d0e2cfacef08
lliolla <lliolla@hotmail.com>
2023-05-30 22:08:42 +0200

    STYLE: fixe bug postion des cartes sur la grille

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,25 +1,19 @@
 const MemoryCard = ({ card, handleChoice, flipped }) => {
-    const handleClick = () => {
-      handleChoice(card);
-    };
-    return (
-      <div className="card">
-    <div className={`inner ${flipped ? 'flipped' : ''}`}>
-      <div className="card-content">
+  const handleClick = () => {
+    handleChoice(card);
+  };
+  return (
+
+      <div className={`memory-card ${flipped ? "flipped" : ""}`}>
         <div className="card-face">
           <img src={card.src} alt="" />
         </div>
-        <div className="card-back" 
-        onClick={handleClick}>
-          <img  src="../../public/assets/images/memory/fan.png" alt="" />
+        <div className="card-back" onClick={handleClick}>
+          <img src="../../public/assets/images/memory/fan.png" alt="" />
         </div>
       </div>
-    </div>
-  </div>
-  
-     
-    );
-  };
-  
-  export default MemoryCard;
-  
+ 
+  );
+};
+
+export default MemoryCard;
