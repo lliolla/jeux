164059beadc4dad517db61df810f075d623d75b5
lliolla <lliolla@hotmail.com>
2023-06-06 23:12:47 +0200

    FEAT : fonction addition

--- src/components/MathCard.jsx
+++ src/components/MathCard.jsx
@@ -1,7 +1,30 @@
-
-
 export const MathCard = () => {
+    const numberOne = 1
+    const numberTwo = 9
+    const operation =(num1,num2)=>{
+        return num1 + num2
+    }
+    const result =operation(numberOne,numberTwo)
+ 
   return (
-    <div>MathCard</div>
-  )
-}
+    <div>
+      <section className="content">
+        <div className="numberOne">
+          <p> {numberOne} </p>
+        </div>
+        <div className="sign">+</div>
+        <div className="numberTwo">
+          <p>{numberTwo}</p>
+        </div>
+      </section>
+      <section className="content-result">
+        <div className="equal">=</div>
+        result   {result}
+        <div className="user-result">
+        
+          <input type="text" name="user-result" id="user-result" />
+        </div>
+      </section>
+    </div>
+  );
+};
