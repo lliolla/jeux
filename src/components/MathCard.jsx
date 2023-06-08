88ed9fcebe1849f787a84a73044f7105719814d8
lliolla <lliolla@hotmail.com>
2023-06-08 23:30:28 +0200

    FEAT: recupeer le resultat et la saisie

--- src/components/MathCard.jsx
+++ src/components/MathCard.jsx
@@ -1,39 +1,55 @@
-import { useRef,useState } from "react";
+
+import { useRef, useState } from "react";
 
 export const MathCard = () => {
-    const numberOne = Math.floor(Math.random()*20+1)
-    const numberTwo = Math.floor(Math.random()*20+1)
-    const inputValue = useRef()
+    const numberOne = generateNumber(20);
+    const numberTwo =  generateNumber(20);
     const [result, setResult] = useState("");
+    const [hideMsg, setHideMsg] = useState(true); 
+    const inputValue = useRef();
 
-    
-  const sendResult = () => {
+    function generateNumber(max) {
+    return Math.floor(Math.random() * max + 1);
+   } 
+   
+
+
+  
+
+   const sendResult = () => {
     const userResult = parseInt(inputValue.current.value);
-    console.log("userResult",userResult);
-    if (!isNaN(userResult)) {
-      const additionResult = numberOne + numberTwo;
-      setResult(additionResult.toString());
-    }
-
- 
+    const correctAnswer =numberOne + numberTwo
+    setResult(correctAnswer.toString());
+    
+    console.log("userResult",userResult,result)
+    
+//decalage entre resultta et ce qui est entré
+    inputValue.current.value = ""; 
   };
 
   return (
     <div className="template-game">
-       <div className="msg">
-        <div className="win-msg">
-            <p>Bravo !</p>
-            <div className="lost-msg">
-                <p>Perdu!</p>
+      
+        <div className="msg">
+         
+            <div className="win-msg">
+              <p>Bravo !</p>
             </div>
+        
+            <div className="lost-msg">
+              <p>
+                Perdu ! La bonne réponse est <span>{result}</span>
+              </p>
+            </div>
+      
         </div>
-       </div>
+ 
       <section className="content">
         <div className="numberOne">
-          <p> {numberOne} </p>
+          <p>{numberOne}</p>
         </div>
         <div className="sign">
-            <p>+</p>
+          <p>+</p>
         </div>
         <div className="numberTwo">
           <p>{numberTwo}</p>
@@ -41,15 +57,14 @@ export const MathCard = () => {
       </section>
       <section className="content-result">
         <div className="equal">
-            <p>=</p>
+          <p>=</p>
         </div>
-       <p> result   {result}</p>
-      
         <div className="user-result">
-       
-          <input type="text" name="user-result" ref={inputValue} placeholder="???"/>
+          <input type="text" name="user-result" ref={inputValue} placeholder="???" />
         </div>
-      <button className="button" onClick={sendResult}>Valider</button>
+        <button className="button" onClick={sendResult}>
+          Valider
+        </button>
       </section>
     </div>
   );
--- src/pages/Math.jsx
+++ src/pages/Math.jsx
@@ -6,6 +6,7 @@ const Math = () => {
     const newGame =()=>{
         console.log("newGame")
     }
+    
     return (
       <div className="game-container">
   
