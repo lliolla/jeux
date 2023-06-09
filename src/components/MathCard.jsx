534b666a3c1da979377ee122037801d271488bfe
lliolla <lliolla@hotmail.com>
2023-06-09 10:48:59 +0200

    FEAT:comparaison des nombre( gagné perdu) et mise a jour du jeux a chaque reponse

--- src/components/MathCard.jsx
+++ src/components/MathCard.jsx
@@ -2,26 +2,37 @@
 import { useRef, useState } from "react";
 
 export const MathCard = () => {
-    const numberOne = generateNumber(20);
-    const numberTwo =  generateNumber(20);
-    const [result, setResult] = useState("");
+    const [numberOne, setnumberOne] = useState(generateNumber(20))
+    const [numberTwo, setnumberTwo] = useState(generateNumber(20))
+
+    // const [result, setResult] = useState("");
+    
     const [hideMsg, setHideMsg] = useState(true); 
     const inputValue = useRef();
+    const correctAnswer =numberOne + numberTwo
 
     function generateNumber(max) {
     return Math.floor(Math.random() * max + 1);
+    
    } 
    
-
-
-  
-
    const sendResult = () => {
     const userResult = parseInt(inputValue.current.value);
-    const correctAnswer =numberOne + numberTwo
-    setResult(correctAnswer.toString());
+   
+    // setResult(correctAnswer.toString());
     
-    console.log("userResult",userResult,result)
+    console.log("userResult",userResult,"correctAnswer",correctAnswer)
+    if(userResult===correctAnswer){
+        setHideMsg(true)
+        setnumberOne(generateNumber(20))
+        setnumberTwo(generateNumber(20))
+        console.log("good",hideMsg)
+    }else{
+        setHideMsg(false)
+        setnumberTwo(generateNumber(20))
+        setnumberOne(generateNumber(20))
+        console.log("bad",hideMsg)
+    }
     
 //decalage entre resultta et ce qui est entré
     inputValue.current.value = ""; 
@@ -29,16 +40,16 @@ export const MathCard = () => {
 
   return (
     <div className="template-game">
-      
+      <h3>Trouvez le resultat du calcul suivant :</h3>
         <div className="msg">
-         
-            <div className="win-msg">
+        <div className="win-msg">
               <p>Bravo !</p>
             </div>
-        
+         
+         
             <div className="lost-msg">
               <p>
-                Perdu ! La bonne réponse est <span>{result}</span>
+                Perdu ! La bonne réponse est <span>{correctAnswer}</span>
               </p>
             </div>
       
